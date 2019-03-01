4e61a658293fb642428ae56d8d117700ce4cf28a
BT_1N3_02 <kkhambhla0604@bths.edu>
2019-03-01 08:35:00 -0500

    ;duiohduih

--- src/Activity2/DeckTester.java
+++ src/Activity2/DeckTester.java
@@ -11,6 +11,28 @@ public class DeckTester {
      */
     public static void main(String[] args) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+        String[] ranks = new ranks[12];
+        String[] suits = new suits[4];
+        int[] values = new values[12];
+
+        Deck deck1 = new Deck(ranks, suits, values);
+
+        //Print list
+        /*for(int i=0; i< Deck.cards.size(); i++)
+        {
+            System.out.println(cards.get(i));
+        }*/
+        System.out.println(deck1);
+
+        //Shuffle
+        deck1.shuffle();
+
+        //Print list again
+        /*for(int i=0; i< cards.size(); i++)
+        {
+            System.out.println(cards.get(i));
+        }*/
+        System.out.println(deck1);
     }
 }
 
